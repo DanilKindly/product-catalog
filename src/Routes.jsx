5d5d1c4107a7d8f1871f3ce6042c279f2d8bfabe
DanilKindly <160669541+DanilKindly@users.noreply.github.com>
2025-07-31 20:01:55 +0500

    Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Import page components
 import CreateProductCardPage from './pages/CreateProductCard';
 
 const AppRoutes = () => {
